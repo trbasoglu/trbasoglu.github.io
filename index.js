d1b7a38cb3842dcfa1f23e468597407e90610eba
trbasoglu <trbasoglu@outlook.com>
2022-07-17 23:55:50 +0300

    New ui added

---
+++ index.js
@@ -0,0 +1,367 @@
+var workExperiences = {
+    "PulpoAR":{
+        "role":"Computer Vision Engineer & RD Team Lead",
+        "logo":"images/pulpoar__logo.svg",
+        "accent-color":"#5B0097",
+        "timeline":"Nov 2019-now",
+        "exp":[
+            "I take critical roles in company and company make exit less than 3 year.",
+            "Machine learning model development on face landmark and segmentation.",
+            "Developing Labelling tools.",
+            "Ml model and algorithm deployment on different environments like Web client(tfjs, emscripten wasm), server(aws lambda, gcp cloud run&functions), apps)",
+            "Developing image processing algorithms for try-on solutions and professional photo edit app.",
+            "Flutter app development",
+            "Development of first versions of virtual try-on plugin"
+        ],
+        "location":"İzmir, Turkiye"
+    },
+    "NKR Software":{
+        "role":"Machine Learning - Intern",
+        "logo":"images/nkrlogo.png",
+        "accent-color":"#F3CE1F",
+        "timeline":"Feb-Mar 2018",
+        "exp":[
+            "Plate detection and recognition software developed.",
+            "Optical Character recognition system developed.",
+            "Data Labelling & analysing."
+        ],
+        "location":"Ankara, Turkiye"
+    },
+    "Mentornity":{
+        "role":"Android Developer - Intern",
+        "logo":"images/mentornity.png",
+        "accent-color":"#B00021",
+        "timeline":"Jul-Aug 2018",
+        "exp":[
+            "Take role on developing Ecosystem Feed application.",
+            "OneSignal, Firebase, linkedin Api are used.",
+            "Playstore app publish processes are learned."
+        ],
+        "location":"İstanbul, Turkiye"
+    }
+}
+
+var projects = [
+    {
+        "image":"images/teknofest.png",
+        "title":"Teknofest AI 2019",
+        "exp":[
+            "Final aşamasında işlemci ile yarışılmış ve başarı yüzdesinde 17. Toplam puanda 15. olduk.",
+        "25 takımdan oluşan kendi grubumuzda ilk gün 11. ikinci gün ise 4. olduk. Finale kalan 50 grup içinde ise ilk gün 21. İkinci gün 13. olduk.",
+        "Nesne tespitinde RFCN,RCNN,SSD,YOLO,FPN modellerinin araştırılması ve karşılaştırılması yapıldı",
+        "RFCN,RCNN ve yolo için eğitim ve testler yapıldı.",
+        "Tensorflow, openCv, sklearn, darknet gibi araç ve kütüphaneler kullanıldı.",
+        "Yaklaşık 1500 görsel etiketlendi.",
+        "Veri çeşitli metodlar uygulanarak çoğaltıldı."]
+    },
+    {
+        "image":"images/pexels-pixabay-267350.jpg",
+        "title":"Android Apps",
+        "exp":["I've developed more than 10 apps.",
+        "I publish my apps on Google play Store",
+        "I've developed with native Android Sdk(Java) and Flutter(Dart)"]
+    }
+]
+
+var cv_projects = [
+    {
+        "image":"/images/city.jpeg",
+        "title":"Prediction Of Life Qualities",
+        "exp":["Project aims to predict life quality of city by using numerical data.",
+        "Project blogged every week on medium.",
+        "85 features(Hospital, school,park numbers per capita, etc.) is extracted from json files.",
+        "Classification and regression  models were developed.",
+        "SVM, Decision Tree, Random forest algorithms used."]
+    },
+    {
+        "image":"images/car-road.jpg",
+        "title":"WAD Video segmentation",
+        "exp":[
+            "It is a segmentation task contains cars,roads,sky and etc.",
+            "U-net model is used.",
+            "30GB(20 traing-10test) data used for train and evaluation.",
+            "Keras ve tensorflow is used."
+        ]
+    },
+    {
+        "image":"images/drone.jpg",
+        "title":"Graduation project",
+        "exp":[
+            "It is a UAV project includes Remote Pilot Station(RPS), Fly control system(FCS) and object tracking systems.",
+            "UAV could not completed because of fly accident.",
+            "Data communication between Raspberry pi-RPS ",
+            "Tracking Algorithms(MIL, KCF, TLD, MEDIANFLOW, GOTURN, MOSSE,CSRT) research and application",
+            "Data production for car detection",
+            "Tests on raspberry pi."
+        ]
+    },
+    {
+        "image":"images/plate.jpg",
+        "title":"Intern/Plate Recognition System",
+        "exp":[
+            "Project completed in 3 weeks.",
+            "Plate Detection&Recognition system developed.",
+            "Data preperation and OCR system development is done because of tesseract not stable enough.",
+            "1200 images labelled and trained.",
+            "Rcnn and yolo models used.",
+            "Darknet, Keras, OpenCV were used."
+        ]
+    },
+    
+    {
+        "image":"images/teknofest.png",
+        "title":"Teknofest AI 2019",
+        "exp":["We qualified for the finals for the first 50 teams and placed 15th.",
+        "RFCN,RCNN,SSD,YOLO and FPN models research and comparison were done.",
+        "RFCN,RCNN and yolo models trained and evaluated.",
+        "Tensorflow, openCv, sklearn, darknet were used."]
+    },
+    {
+        "image":"images/captcha_sample.jpg",
+        "title":"Captha Resolve Algorithm",
+        "exp":[
+            "Random symbols are placed radom place on random image with random color and ratio.Task is find places of symbols given order.",
+            "Template matching, surf matching, object detection and classification algorithms used to solve problem.",
+            "Above algorithms used together by voting mechanism.",
+            "Efficientnet is used because of size and speed concerns."
+        ]
+    },
+]
+var techStack = {
+    "Computer Vision":[
+        {"name":"OpenCV","logo":"images/techstack/opencv.png"},
+        {"name":"Keras","logo":"images/techstack/Keras_Logo.jpg"},
+        {"name":"Tensorflow","logo":"images/techstack/icons8-tensorflow-48.png"},
+        {"name":"Sklearn","logo":"images/techstack/scikit-learn-logo-8766D07E2E-seeklogo.com.png"},
+        {"name":"Numpy","logo":"images/techstack/numpy-logo-479C24EC79-seeklogo.com.png"},
+        {"name":"Tfjs","logo":"images/techstack/Tensorflow_logo.svg"},
+        {"name":"Tflite","logo":"images/techstack/Tensorflow_logo.svg"},
+        {"name":"OnnxRuntime","logo":"images/techstack/ONNX-Runtime-logo.svg"},
+        {"name":"Python","logo":"images/techstack/icons8-python-48.png"},
+        {"name":"C++","logo":"images/techstack/icons8-c++-48.png"},
+        {"name":"JavaScript","logo":"images/techstack/icons8-javascript-48.png"},
+        {"name":"Aws Sagemaker&Groundtruth","logo":"images/techstack/icons8-amazon-web-services-48.png"},
+    ],
+    "Mobile":[
+        {"name":"Flutter","logo":"images/techstack/icons8-flutter-48.png"},
+        {"name":"Native Android","logo":"images/techstack/icons8-android-os-48.png"},
+        // {"name":"React Native","logo":""},
+        {"name":"Dart","logo":"images/techstack/icons8-dart-48.png"},
+        {"name":"Java","logo":"images/techstack/icons8-java-48.png"},
+        {"name":"Firebase","logo":"images/techstack/icons8-firebase-48.png"}
+    ],
+    "Web":[
+        {"name":"Django","logo":"images/techstack/icons8-django-48.png"},
+        {"name":"AWS","logo":"images/techstack/icons8-amazon-web-services-48.png"},
+        {"name":"GCP","logo":"images/techstack/icons8-google-cloud-48.png"},
+        {"name":"Fast Api","logo":"images/techstack/fastapi.png"},
+        {"name":"Serverless","logo":"images/techstack/serverless dark.svg"},
+        {"name":"Python","logo":"images/techstack/icons8-python-48.png"},
+        {"name":"Html/Css","logo":"images/techstack/icons8-html-5-48.png"},
+        {"name":"JavaScript","logo":"images/techstack/icons8-javascript-48.png"}
+    ],
+    "Other":[
+        {"name":"C#","logo":"images/techstack/icons8-c-sharp-logo-48.png"},
+        {"name":"Adobe XD","logo":"images/techstack/icons8-adobe-xd-48.png"},
+        {"name":"Admob","logo":"images/techstack/icons8-google-admob-48.png"},
+        {"name":"Play Console","logo":"images/techstack/icons8-google-play-48.png"},
+        {"name":"Android Studio","logo":"images/techstack/icons8-android-studio-48.png"},
+        {"name":"Linux","logo":"images/techstack/icons8-linux-48.png"},
+        {"name":"Unity","logo":"images/techstack/icons8-unity-50.png"},
+        {"name":"Markdown","logo":"images/techstack/icons8-markdown-50.png"},
+        {"name":"Git","logo":"images/techstack/icons8-git-48.png"},
+        {"name":"Web Assembly with Emscripten","logo":"images/techstack/WebAssembly_Logo.svg.png"},
+    ]
+}
+
+var apps = [
+    {"name":"Bil Bakalım",
+    "exp":"It is a word game.Keyboard is created for game. Animations are highly used.",
+    "Tech":"Flutter",
+    "Store Link":"https://play.google.com/store/apps/details?id=com.hisario.bilbakalim&hl=tr&gl=US",
+    "icon":"https://play-lh.googleusercontent.com/cX3rzUVfMxfPm7v9eheOIJFjmE5UEWGdaPHwHOTDSO88FnvRrxBD4A_qFtfxjg4MwZDZ=s64-rw"},
+    {"name":"Times Up - Screen on time viewer, floating window",
+    "exp":"It is an app to prevent over usage phone. It displays passed time on floating window.",
+    "Tech":"Native Android with Java",
+    "Store Link":"https://play.google.com/store/apps/details?id=com.hisario.hisario.sayac",
+    "icon":"https://play-lh.googleusercontent.com/rhq3RDd3p5eIyLi6HXk2E8PKJulp1lQsJt6-tdaBBLr3ytUbrCndFz5eukmLfUW14B4=s64-rw"},
+    {"name":"Today's Quote",
+    "exp":"This app shows random 5 quotes every day. The floating window is used to show quotes.",
+    "Tech":"Native Android with Java",
+    "Store Link":"https://play.google.com/store/apps/details?id=com.hisario.todaysquote&hl=en&gl=US",
+    "icon":"https://play-lh.googleusercontent.com/jNWwxS-zGOO0b_AxTKbVR-9uPtvirOXoXD7tclJ5Uh585wzQ2631qamKXkyTi2mzWg=s64-rw"},
+    {"name":"Tyt Kimya - Hızlı Sorular",
+    "exp":"It is a education app for chemistry.It is a last native android app that I have developed.",
+    "Tech":"Native Android with Java",
+    "Store Link":"https://play.google.com/store/apps/details?id=com.hisario.tytkimya&hl=en&gl=US",
+    "icon":"https://play-lh.googleusercontent.com/Bs_0Axivrz_fgD4txoJCBmm_EdQkg8JMTAWQdtHUcVLr-xp8Mz2Pf0F1SIf2lrmiQds=s64-rw"},
+    {"name":"Radion",
+    "exp":"It is a radio app supports background play, widget, and notification controls.",
+    "Tech":"Native Android with Java Backend",
+    "Store Link":"",
+    "icon":"images/radyon.png"},
+    {"name":"Tozlu Kelimeler",
+    "exp":"It is similar app with Today's Quote. It shows old turkish words that not used today as before.",
+    "Tech":"Native Android with Java Backend",
+    "Store Link":"",
+    "icon":"images/tozlu_keli,meler.png"},
+    {"name":"Ekolog",
+    "exp":"Currency track app. It supports different type of widgets.",
+    "Tech":"Native Android with Java",
+    "Store Link":"",
+    "icon":"images/ekolog.png"},
+    {"name":"Free Games of week",
+    "exp":"Simple app that shows free games for limited time. Aws lambda and firebase is used to track free games.",
+    "Tech":"Flutter",
+    "Store Link":"",
+    "icon":"images/freegames.png"},
+    {"name":"Kolay Tercih",
+    "exp":"It is an app that hold university and program details. ",
+    "Tech":"Flutter",
+    "Store Link":"https://play.google.com/store/apps/details?id=com.hisario.tercihrehberi&hl=tr&gl=US",
+    "icon":"https://play-lh.googleusercontent.com/Q8Yk38uRSgrZJoK74W9HTiSdIwv6wL9KoXgNYFtIxBnH5KdY3wi3wpobTMcLZVr7xgQ=s64-rw"},
+    {"name":"TicTacToe",
+    "exp":"It is a simple tictactoe game.",
+    "Tech":"Flutter",
+    "Store Link":"https://play.google.com/store/apps/details?id=tictactoe.hisario.com.tictactoe&hl=tr&gl=US",
+    "icon":"https://play-lh.googleusercontent.com/3R8eMQKF5QsoATaIo26wX-zyO-4eNBe9wGSa64G_jZ6Yz71VfNDQtjTl9zr5Zq7rj_4=s64-rw"},
+    {"name":"AYT Edebiyat yazar bilgi eser",
+    "exp":"It is a another exam work app.",
+    "Tech":"Flutter",
+    "Store Link":"https://play.google.com/store/apps/details?id=hisario.com.yazar_eser&hl=tr",
+    "icon":"https://play-lh.googleusercontent.com/iztvSbgFZ9HwVfoDwWzjhZM_MME1wrv96rFuPxOdD_e0be9HJt0mIA4PuxBQakuK7A=s64-rw"},
+    {"name":"Notification Track",
+    "exp":"It is an app to list and track notification.It becomes default Android feature while I'm developing an app.",
+    "Tech":"Flutter",
+    "Store Link":"",
+    "icon":"images/bildirim.png"},
+    
+]
+
+function load_tech_stack(){
+    techstack_container = document.getElementById("techstack_container")
+    for (var key in techStack){
+        var title = document.createElement("h4")
+        title.classList = ["title is-4"]
+        title.innerHTML = key
+        techstack_container.appendChild(title);
+        var techdiv = document.createElement("div")
+        techdiv.classList = ["tags are-medium"]
+        for(var index in techStack[key]){
+            var techspan = document.createElement("span")
+            techspan.classList = ["tag is-rounded"]
+            techspan.innerHTML = '<figure class="image is-16x16"><img src="'+techStack[key][index]["logo"]+'"></figure>'+techStack[key][index]["name"]
+            //techspan.innerHTML = techStack[key][index]["name"]
+            techdiv.appendChild(techspan)
+        }
+        techstack_container.appendChild(techdiv)
+      }
+}
+
+function load_work_experiences(){
+    var work_experiences_container = document.getElementById("work_experiences")
+    for(var key in workExperiences){
+        var companydiv = document.createElement("div")
+        companydiv.classList = ["columns"]
+        var companyTimelinediv = document.createElement("div")
+        companyTimelinediv.classList = ["column is-2"]
+        companyTimelinediv.innerHTML = `<h6>${workExperiences[key]["timeline"]}</h6>`
+        var companylogodiv = document.createElement("div")
+        companylogodiv.classList= ["column"]
+        companylogodiv.innerHTML = `<figure class="image is-64x64"><img src="${workExperiences[key]["logo"]}"></figure>`
+        var companyInfodiv = document.createElement("div")
+        companyInfodiv.classList = ["column is-10"] 
+        var experienceList = document.createElement("ul")
+        experienceList.classList = ["subtitle is-6"]
+        var companyInfoInnerHtml = `<h4 class="title is-4" style="color:${workExperiences[key]["accent-color"]}">${key}</h1>
+                <h5 class="subtitle is-6" >${workExperiences[key]["role"]}</h1>
+                <p class="subtitle is-6">
+                    <ul style="color: white;">`
+        for(var experience in workExperiences[key]["exp"]){
+            companyInfoInnerHtml += `<li>${workExperiences[key]["exp"][experience]}</li>`
+        }
+        companyInfoInnerHtml+=`</ul></p>`
+        companydiv.appendChild(companyTimelinediv)
+        companydiv.appendChild(companylogodiv)
+        companyInfodiv.innerHTML = companyInfoInnerHtml
+        companydiv.appendChild(companyInfodiv)
+        work_experiences_container.appendChild(companydiv)
+        console.log(key)
+    }
+}
+
+function load_projects(id, projects){
+    var projects_container = document.getElementById(id)
+    for(var key in projects){
+        var projectsdiv = document.createElement("div")
+        projectsdiv.classList = ["columns"]
+        var imagediv = document.createElement("div")
+        imagediv.classList= ["column"]
+        imagediv.innerHTML = `<figure class="image "><img src="${projects[key]["image"]}"></figure>`
+        var projectInfodiv = document.createElement("div")
+        projectInfodiv.classList = ["column is-10"] 
+        var experienceList = document.createElement("ul")
+        experienceList.classList = ["subtitle is-6"]
+        var projectInfoInnerHtml = `<h4 class="title is-4 green">${projects[key]["title"]}</h1>
+                <p class="subtitle is-6">
+                    <ul style="color: white;">`
+        for(var experience in projects[key]["exp"]){
+            projectInfoInnerHtml += `<li>${projects[key]["exp"][experience]}</li>`
+        }
+        projectInfoInnerHtml+=`</ul></p>`
+        projectsdiv.appendChild(imagediv)
+        projectInfodiv.innerHTML = projectInfoInnerHtml
+        projectsdiv.appendChild(projectInfodiv)
+        projects_container.appendChild(projectsdiv)
+        console.log(key)
+    }
+}
+function load_home(){
+    load_work_experiences()
+    load_projects("projects", projects);
+    load_tech_stack();
+    }
+
+function load_computer_vision_page(){
+    load_projects("computerVisionProjects",cv_projects)
+}
+function select_app(i){
+    appname = document.getElementById("appname")
+    appname.innerHTML = apps[i]["name"]
+    appcontent = document.getElementById("appcontent")
+    appcontent.innerHTML = apps[i]["exp"]
+    apptech = document.getElementById("app_tech")
+    apptech.innerHTML = apps[i]["Tech"]
+    if(apps[i]["Tech"]==="Flutter"){
+        apptech.classList= ["tag is-info"]
+    }else{
+        apptech.classList= ["tag is-primary"]
+    }
+    storelink = document.getElementById("storelink")
+    storelink.href = apps[i]["Store Link"]
+    if(apps[i]["Store Link"].length>0){
+        storelink.style.visibility ="visible"
+    }else{
+        storelink.style.visibility ="hidden"
+    }
+    appIcons = document.getElementById("appIcons")
+    for(var ix=0;ix<appIcons.children.length;ix++){
+        appIcons.children[ix].style.filter = "grayscale(100%)"
+    }
+    appIcons.children[i].style.filter = "grayscale(0%)"
+}
+function load_apps(){
+    appsDiv = document.getElementById("apps");
+    appsDivInnerHtml = appsDiv.getInnerHTML();
+    appsDivInnerHtml += `<span class="tags are-medium" id="appIcons">`
+    for(var index in apps){
+        var app = apps[index]
+        appsDivInnerHtml+= `<figure class="image is-96x96" style="padding:5px; filter: grayscale(100%);" >
+                <button style="background:transparent; border-style:none" onclick="select_app(${index})"><image src="${app["icon"]}" style="border-radius: 10%;"></image></button>
+        </figure>`
+    }
+    
+    appsDivInnerHtml+="</span>"
+    appsDiv.innerHTML = appsDivInnerHtml;
+    select_app(0);
+}
